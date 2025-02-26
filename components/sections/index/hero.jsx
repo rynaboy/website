961e68e0dafb85acf228d980ca907f9e1eabf2fe
molryna <154453026+rynaboy@users.noreply.github.com>
2025-02-26 11:45:48 +0000

    cahnge hero

--- components/sections/index/hero.jsx
+++ components/sections/index/hero.jsx
@@ -65,11 +65,11 @@ export default function Hero() {
 				</section>
 				<section>
 					<button	className={`button ${button.primary}`}
-							onClick={ () => window.location = 'mailto:hello@andrewnelson.net' } >
+							onClick={ () => window.location = 'mailto:rynaboy22@gmail.com' } >
 						{content.buttons.primary.title}
 					</button>
 					<button className={`button ${button.secondary} leaveSite`}
-							onClick={ ()=> window.open("https://www.linkedin.com/in/--andrewnelson/", "_blank") } >
+						onClick={() => window.open("https://www.linkedin.com/in/mol-ryna-2296b51a1/", "_blank") } >
 						{content.buttons.secondary.title}
 					</button>
 				</section>
