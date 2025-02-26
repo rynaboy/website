09a15974f3a316b66ff5c6366427d311496ab4f5
molryna <154453026+rynaboy@users.noreply.github.com>
2025-02-26 12:21:12 +0000

    cahnge suppy co

--- components/sections/index/career.jsx
+++ components/sections/index/career.jsx
@@ -32,13 +32,13 @@ export default function Career() {
 					<article className={career.company}>
 						<div className={career.companyContent}>
 							<span className={career.companyHeader}>
-								<h3>My Supply Co.</h3>
+								<h3>Technology Solution Development Co.Ltd</h3>
 								<h4>Permanent Full-time</h4>
-								<h4>Apr 2019 - Present · 3 yrs 10 mos</h4>
-								<h5>Vancouver, British Columbia, Canada</h5>
+								<h4>Apr 2019 - Present · 6 years 1 months</h4>
+								<h5>Pnhom Penh, Cambodia</h5>
 							</span>
 							<p>
-							My Supply Co. helps Canadians manage mental and physical health with naturally occurring nootropic and adaptogenic products. They carry products with complex attributes, aiming to solve a large variety of personal care needs — this requires an extremely customized and evolving approach to how the store is built and functions.
+							Technology Solution Development Co.Ltd process the skills and powess to develop an amazing software products which delinetes the client's organization and satiates their customer's requirements.
 							</p>
 						</div>
 						<div className={career.companyAlt}></div>
