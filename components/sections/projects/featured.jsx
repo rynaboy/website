15037044535002b1a5c3cd8ebb50cfae2cc0844f
molryna <154453026+rynaboy@users.noreply.github.com>
2025-02-27 16:26:24 +0000

    no featuer

--- components/sections/projects/featured.jsx
+++ components/sections/projects/featured.jsx
@@ -15,27 +15,7 @@ export default function FeaturedProjects() {
 
 	return (
 		<Section classProp={css.hasBg}>	
-			<Container spacing={'verticalXXXXLrg'}>
-				<SectionTitle
-					title="Featured Projects"
-					preTitle="UX and Full Stack"
-					subTitle="Focused on the experience, driven by the engineering."
-				/> 				{
-				content.map( (data, index) => {
-					return (
-						<FeaturedProject content={data} index={index} key={index} />
-					)
-				})
-				}
-			</Container>
-			<div className={css.bgContainer}>
-				<span className={css.orbitalBg}>
-					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroLeft} ${css.heroOrbital}`}></span></span>
-					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroCenter}`}></span></span>
-					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroRight} ${css.heroOrbital}`}></span></span>
-				</span>
-				<span className={css.afterGlowBg}></span>
-			</div>
+			
 		</Section>
 	)
 }
