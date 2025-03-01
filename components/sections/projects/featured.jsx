05f06f2597b03a17c1d8da415b305b3f00dbe1b0
molryna <154453026+rynaboy@users.noreply.github.com>
2025-03-01 09:55:24 +0000

    add featured

--- components/sections/projects/featured.jsx
+++ components/sections/projects/featured.jsx
@@ -2,24 +2,40 @@ import FeaturedProject from '../../blocks/projects/featured'
 
 
 // Section structure
-import Section 		from '../../structure/section';
-import Container 	from '../../structure/container';
-import Badges 		from '../../utils/badge.list.util'
-import Icon 		from '../../utils/icon.util'
+import Section from '../../structure/section';
+import Container from '../../structure/container';
+import Badges from '../../utils/badge.list.util'
+import Icon from '../../utils/icon.util'
 import SectionTitle from '../../blocks/section.title.block'
 
-import css 			from '../../../styles/sections/projects/featured.module.scss'
-import content 		from '../../../content/projects/featured.json'
+import css from '../../../styles/sections/projects/featured.module.scss'
+import content from '../../../content/projects/featured.json'
 
 export default function FeaturedProjects() {
 
 	return (
-		<Section classProp={css.hasBg}>	
-<<<<<<< HEAD
-			 
-=======
-			
->>>>>>> refs/remotes/origin/main
+		<Section classProp={css.hasBg}>
+			<Container spacing={'verticalXXXXLrg'}>
+				<SectionTitle
+					title="Featured Projects"
+					preTitle="UX and Full Stack"
+					subTitle="Focused on the experience, driven by the engineering."
+				/> 				{
+					content.map((data, index) => {
+						return (
+							<FeaturedProject content={data} index={index} key={index} />
+						)
+					})
+				}
+			</Container>
+			<div className={css.bgContainer}>
+				<span className={css.orbitalBg}>
+					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroLeft} ${css.heroOrbital}`}></span></span>
+					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroCenter}`}></span></span>
+					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroRight} ${css.heroOrbital}`}></span></span>
+				</span>
+				<span className={css.afterGlowBg}></span>
+			</div>
 		</Section>
 	)
 }
